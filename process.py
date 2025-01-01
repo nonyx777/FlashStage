b218f53ebedc3c1bd430b83a9d5bbc468a92de0b
nonyx777 <nahomretebo@gmail.com>
2025-01-01 22:07:40 +0300

    finding the most lit area in the image

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
 
-def process_image(image_path, color, image_num, resultant_array):
+def relight_image(image_path, color, image_num, resultant_array):
     image = Image.open(image_path).convert("RGB")
     image_array = np.array(image)
     modified_array = image_array * color
@@ -9,3 +9,28 @@ def process_image(image_path, color, image_num, resultant_array):
     modified_image = Image.fromarray(modified_array)
     modified_image.save(f"image{image_num}.png")
     resultant_array += modified_array
+
+def most_lit_area(image_path: str):
+    image = Image.open(image_path).convert("RGB")
+    image_array = np.array(image)
+    treshold: float = 1.0
+    #calculate magnitude of each color in the matrix
+    pixel_norms = np.linalg.norm(image_array, axis=2) / 255
+    bright_mask = pixel_norms < treshold
+    #only colors with magnitude above the specified
+    #treshold are left
+    image_array[bright_mask] = np.array([0, 0, 0])
+    #sum(weight * index), where weight is the color magnitude
+    #and index as the pixel location
+    weight_x_index = np.zeros((2, ))
+    for row in range(image_array.shape[0]):
+        for col in range(image_array.shape[1]):
+            weight_x_index += pixel_norms[row, col] * np.array([row, col])
+    #sum(weight)
+    weight = 0
+    for row in range(image_array.shape[0]):
+        for col in range(image_array.shape[1]):
+            weight += pixel_norms[row, col]
+    average_index = np.int64(np.floor(result1 / result2))
+    return average_index
+
